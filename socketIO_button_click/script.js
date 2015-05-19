57fe36a10b2980ad3c0bf794e67030bd00e1eecc
ClementR <rondeau.clement@gmail.com>
2015-05-19 23:23:23 +0200

    Add new socketIO base project, trying to get it working

---
+++ socketIO_button_click/script.js
@@ -0,0 +1,25 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example',
+                           {preload: preload, create: create, update: update});
+var socket;
+function preload() {
+    game.load.image('button', '../assets/images/boxItem.png');
+}
+
+function create() {
+    game.stage.backgroundColor = 0x337799;
+
+    socket = io();
+    var sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'button');
+    sprite.inputEnabled = true;
+    sprite.events.onInputDown.add(on_click, this);
+
+    /*socket.on('chat message', function(msg){
+    });*/
+}
+
+function update() {
+}
+
+function on_click() {
+    socket.emit('button_click', 'Button clicked !');
+}
---
+++ socketIO_button_click/server.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/script.js');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('Connection of a client.');
+    socket.on('button_click', function(msg){
+        console.log(msg);
+        //io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
