5981c400ba39273c044e1e5030eb17d9ef783c0f
ClementR <rondeau.clement@gmail.com>
2015-05-24 13:00:03 +0200

    Working on sprite to sprite movement

--- launch_bitmapText_to_bitmapText/script.js
+++ launch_bitmapText_to_bitmapText/script.js
@@ -10,15 +10,16 @@ function preload() {
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    game.stage.backgroundColor = 0x337799;
-    b_word = game.add.bitmapText(350, 10, 'carrier_command', 'FUCK', 17);
-    g_word = game.add.bitmapText(350, 400, 'carrier_command', 'PEACE', 17);
+    create_controls();
 
+    game.stage.backgroundColor = 0x337799;
+    b_word = game.add.bitmapText(150, 10, 'carrier_command', 'FUCK', 17);
+    g_word = game.add.bitmapText(350, 400, 'carrier_command', 'PEACE', 17);
+    g_word.is_launched = false;
     game.physics.arcade.enable([b_word, g_word]);
 
     b_word.body.collideWorldBounds = true;
     g_word.body.collideWorldBounds = true;
-    launch_word();
 }
 
 function update() {
@@ -26,8 +27,29 @@ function update() {
         b_word.kill();
         g_word.kill();
     });
+
+    b_word.body.velocity.setTo(0, 20);
+
+    if(g_word.is_launched) {
+        // g_word.body.velocity.setTo();
+        game.physics.moveToObject(g_word, b_word, 100, 2000);
+    }
+
+    if(spacebar.isDown){
+        launch_word(g_word);
+    }
 }
 
 function launch_word(word) {
-    b_word.body.velocity.setTo(0, 500);
+    // Calculate distance between the two words
+    g_word.body.velocity.setTo(0, -500);
+    g_word.is_launched = true;
+}
+
+function create_controls() {
+    up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
