6a1a74d6853624c9d22ac40635acde78f1e169c9
C.RONDEAU <clement.rondeau@lengow.com>
2015-05-28 14:36:44 +0200

    Start to work on rotation of the cursor around the circle

--- random_blood_splatter/script.js
+++ random_blood_splatter/script.js
@@ -7,11 +7,11 @@ function preload() {
 function create() {
     game.stage.backgroundColor = 0x2c3e50;
 
-    var circle = game.add.graphics(0, 0);
+    circle = game.add.graphics(0, 0);
     circle.beginFill(0xFFFFFF, 1);
     circle.drawCircle(game.world.centerX, game.world.centerY, 100);
 
-    var cursor = game.add.graphics(game.world.centerX - 50, game.world.centerY);
+    cursor = game.add.graphics(game.world.centerX - 150, game.world.centerY);
     cursor.beginFill(0xFFFFFF);
     cursor.moveTo(-5, 0);
     cursor.lineTo(-30, 20);
@@ -20,4 +20,6 @@ function create() {
 }
 
 function update() {
+    // cursor.rotation = game.physics.arcade.angleBetween(cursor, circle.position);
+    cursor.rotation = 0;
 }
