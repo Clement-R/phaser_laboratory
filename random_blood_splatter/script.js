837e8d16ce556cc9776702c2a9e5b5bd8d4f0eef
ClementR <rondeau.clement@gmail.com>
2015-05-28 23:29:02 +0200

    Get orbital working but still experimental

--- random_blood_splatter/script.js
+++ random_blood_splatter/script.js
@@ -1,28 +1,42 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example',
                            {preload: preload, create: create, update: update});
-
 function preload() {
 }
 
 function create() {
     game.stage.backgroundColor = 0x2c3e50;
+    create_controls();
 
     circle = game.add.graphics(game.world.centerX, game.world.centerY);
     circle.beginFill(0xFFFFFF, 1);
     circle.drawCircle(0, 0, 100);
-    circle.inputEnabled = true;
-    circle.input.enableDrag(true);
+
 
     cursor = game.add.graphics(game.world.centerX - 150, game.world.centerY);
-    cursor.beginFill(0xFFFFFF);
-    cursor.moveTo(-5, 0);
-    cursor.lineTo(-30, 20);
-    cursor.lineTo(-30, -20);
-    cursor.endFill();
+    cursor.beginFill(0xFFFFFF, 1);
+    cursor.drawCircle(0, 0, 10);
     cursor.inputEnabled = true;
     cursor.input.enableDrag(true);
+    cursor.rotation = game.physics.arcade.angleBetween(cursor, circle);
 }
 
 function update() {
-    cursor.rotation = game.physics.arcade.angleBetween(cursor, circle.position);
+    
+    cursor.x = circle.x + (50 + 150) * Math.cos(cursor.rotation);
+    cursor.y = circle.y + (50 + 150) * Math.sin(cursor.rotation);
+    
+    //cursor.rotation = game.physics.arcade.angleBetween(cursor, circle);
+    if(up.isDown) {
+        cursor.rotation += 0.05;
+    } else if (down.isDown) {
+        cursor.rotation -= 0.05;
+    }
+}
+
+function create_controls() {
+    up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
