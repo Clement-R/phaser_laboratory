63707f81971bdac3cfee95b27282555e51c28aa3
C.RONDEAU <clement.rondeau@lengow.com>
2015-06-01 16:36:43 +0200

    Start simple jumping character prototype

---
+++ simple_jump/script.js
@@ -0,0 +1,56 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example',
+                           {preload: preload, create: create, update: update});
+
+var player;
+var jumpTimer = 0;
+var jumpButton;
+
+function preload() {
+}
+
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.stage.backgroundColor = 0x2c3e50;
+
+    game.physics.arcade.gravity.y = 300;
+
+    var player_texture = game.add.bitmapData(64, 64);
+    player_texture.ctx.beginPath();
+    player_texture.ctx.rect(0,0,32,32);
+    player_texture.ctx.fillStyle = '#0000ff';
+    player_texture.ctx.fill();
+
+    player = game.add.sprite(32, 320, player_texture);
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+    player.body.collideWorldBounds = true;
+    player.body.gravity.y = 1000;
+    player.body.maxVelocity.y = 500;
+    player.body.setSize(20, 32, 5, 16);
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+}
+
+function update() {
+    // game.physics.arcade.collide(player, layer);
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown) {
+        player.body.velocity.x = -150;
+    } else if (cursors.right.isDown) {
+        player.body.velocity.x = 150;
+    }
+    
+    if(jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
+        player.body.velocity.y = -500;
+        jumpTimer = game.time.now + 750;
+    }
+}
+
+function render () {
+
+    // game.debug.text(game.time.physicsElapsed, 32, 32);
+    // game.debug.body(player);
+    game.debug.bodyInfo(player, 16, 24);
+
+}
