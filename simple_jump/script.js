511182455f05550559cd53629f6605b04f942b1e
Clement-R <rondeau.clement@gmail.com>
2015-06-02 13:11:49 +0200

    Change simple jump to jump on button pressed

--- simple_jump/script.js
+++ simple_jump/script.js
@@ -6,6 +6,7 @@ var jumpTimer = 0;
 var jumpButton;
 var jump = 0;
 var maxJump = 2;
+var speed = 1;
 
 function preload() {
 }
@@ -31,40 +32,43 @@ function create() {
     player.body.setSize(20, 32, 5, 16);
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    sprintButton = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
+
+    jumpButton.onDown.add(function(){
+        if(/* game.time.now > jumpTimer && */ jump < maxJump){
+            if(jump == 0) {
+                console.log('JUMP');
+            } else {
+                console.log('DOUBLE JUMP');
+            }
+            
+            player.body.velocity.y = -500;
+            // jumpTimer = game.time.now + 350;
+            jump += 1;
+        }
+    });
 }
 
 function update() {
-    // game.physics.arcade.collide(player, layer);
     player.body.velocity.x = 0;
 
     if (cursors.left.isDown) {
-        player.body.velocity.x = -150;
+        player.body.velocity.x = -150 * speed;
     } else if (cursors.right.isDown) {
-        player.body.velocity.x = 150;
-    }
-    
-    if(jumpButton.isDown && game.time.now > jumpTimer && jump < maxJump - 1) {
-        player.body.velocity.y = -500;
-        jumpTimer = game.time.now + 500;
-        jump += 1;
+        player.body.velocity.x = 150 * speed;
     }
 
-    console.log(jump);
+    if(sprintButton.isDown) {
+        speed = 2;
+    } else {
+        speed = 1;
+    }
 
     if(player.body.onFloor()) {
         jump = 0;
     }
-
-    /*if(jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
-        player.body.velocity.y = -500;
-        jumpTimer = game.time.now + 750;
-    }*/
 }
 
 function render () {
-
-    // game.debug.text(game.time.physicsElapsed, 32, 32);
-    // game.debug.body(player);
     game.debug.bodyInfo(player, 16, 24);
-
 }
