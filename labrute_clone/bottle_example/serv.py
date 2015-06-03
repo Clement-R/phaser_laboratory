f639b9f3ae821d1a8db444f9e3873be851b0302c
Clement-R <rondeau.clement@gmail.com>
2015-06-03 17:58:12 +0200

    Add Bottle simple example

---
+++ labrute_clone/bottle_example/serv.py
@@ -0,0 +1,14 @@
+from bottle import route, run, template, view
+
+
+run(reloader=True)
+
+
+@route('/')
+@route('/hello')
+@route('/hello/<name>')
+@view('index')
+def hello(name='World'):
+    return dict(name=name)
+
+run(host='localhost', port=8080, debug=True)
