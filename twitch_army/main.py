97b8abad05daf39eb1eb7e7c0798299c76b0f0a4
ClementR <rondeau.clement@gmail.com>
2015-06-08 23:54:30 +0200

    Add method to get viewers amount on a stream by streamer name

--- twitch_army/main.py
+++ twitch_army/main.py
@@ -4,12 +4,16 @@ https://github.com/justintv/Twitch-API/blob/master/authentication.md#auth-code
 """
 
 import requests
+import json
 
+
+def get_viewers_number(user):
+    url_user_stream = '{}/streams/{}'.format(url_base, user)
+    r = requests.get(url_user_stream)
+    infos = json.loads(r.text)
+    return infos['stream']['viewers']
+
+user = "imaqtpie"
 url_base = 'https://api.twitch.tv/kraken/'
-url = '{}/streams/imaqtpie'.format(url_base)
-# headers = {'Authorization: OAuth {}'.format()}
-headers = {}
-
-r = requests.get(url, headers=headers)
-
-print(r.text)
+print(get_viewers_number('imaqtpie'))
+print(get_viewers_number('Voyboy'))
