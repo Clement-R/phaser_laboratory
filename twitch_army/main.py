83ecb12ed333a926d3a0311fb1616e9ddf11c3d8
ClementR <rondeau.clement@gmail.com>
2015-06-08 23:46:28 +0200

    Now list infos about imaqtpie stream, woot

--- twitch_army/main.py
+++ twitch_army/main.py
@@ -6,8 +6,10 @@ https://github.com/justintv/Twitch-API/blob/master/authentication.md#auth-code
 import requests
 
 url_base = 'https://api.twitch.tv/kraken/'
-url = '{}/streams'.format(url_base)
-headers = {'user-agent': 'my-app/0.0.1'}
+url = '{}/streams/imaqtpie'.format(url_base)
+# headers = {'Authorization: OAuth {}'.format()}
+headers = {}
+
 r = requests.get(url, headers=headers)
 
 print(r.text)
