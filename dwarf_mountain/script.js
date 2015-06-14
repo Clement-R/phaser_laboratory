28b6bc3f903ede5f07740c492a0ba5bab4a4850b
ClementR <rondeau.clement@gmail.com>
2015-06-14 20:17:55 +0200

    Working on base

--- dwarf_mountain/script.js
+++ dwarf_mountain/script.js
@@ -13,15 +13,20 @@ function create() {
     game.stage.backgroundColor = 0x2c3e50;
 
     timer = game.time.create(false);
-    timer.loop(2000, update_money, this);
+    timer.loop(1000, update_money, this);
     timer.start();
+
     timer_1 = game.time.create(false);
     timer_1.loop(10000, function(){base += 10;}, this);
     timer_1.start();
+
+    score_text = game.add.text(10, 80, 'Money : ' + money,
+                               { font: '26px Arial', fill: '#fff' });
 }
 
 function update() {
     console.log(money);
+    score_text.text = 'Money : ' + money;
 }
 
 function update_money() {
