d8edd8639d515201250e018e57b496a67f9ee9b7
ClementR <rondeau.clement@gmail.com>
2015-06-28 22:49:52 +0200

    Start to migrate Zombie escape to P2 physic system

--- zombie_escape/script.js
+++ zombie_escape/script.js
@@ -1,20 +1,34 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example',
                            {preload: preload, create: create, update: update});
 
+var collisionCircle;
+
 function preload() {
 }
 
 function create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.p2.gravity.y = 0;
 
     game.stage.backgroundColor = 0x2c3e50;
 
     add_safe_zone();
     add_enemies();
     add_player();
+
+    game.physics.p2.enable(player);
+    player.body.fixedRotation = true;
+    game.physics.p2.enable(baddy);
+    baddy.body.fixedRotation = true;
+
+    game.physics.p2.enable(baddy_radius);
+    baddy_radius.body.setCircle(45);
+    baddy_radius.body.fixedRotation = true;
 }
 
 function update() {
+    // throw new Error("Something went badly wrong!");
+
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
 
@@ -31,16 +45,7 @@ function update() {
         player.body.velocity.y = 150;
     }
 
-    // Player in enemy radius
-    if (game.physics.arcade.overlap(player, baddy_radius)) {
-        game.physics.arcade.moveToObject(baddy, player);
-    } else {
-        baddy.body.velocity.x = 0;
-        baddy.body.velocity.y = 0;
-    }
 
-    baddy_radius.x = baddy.x + (baddy.width / 2);
-    baddy_radius.y = baddy.y + (baddy.height / 2);
 }
 
 function add_safe_zone() {
@@ -50,8 +55,6 @@ function add_safe_zone() {
     safe_texture.ctx.fillStyle = '#ffffff';
     safe_texture.ctx.fill();
     safe_zone = game.add.sprite(500, 320, safe_texture);
-
-    game.physics.enable(safe_zone, Phaser.Physics.ARCADE);
 }
 
 function add_player() {
@@ -62,9 +65,8 @@ function add_player() {
     player_texture.ctx.fill();
 
     player = game.add.sprite(520, 340, player_texture);
-    game.physics.enable(player, Phaser.Physics.ARCADE);
-    player.body.collideWorldBounds = true;
-    player.body.maxVelocity.y = 500;
+    /*player.body.collideWorldBounds = true;
+    player.body.maxVelocity.y = 500;*/
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     sprintButton = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
@@ -73,10 +75,8 @@ function add_player() {
 function add_enemies() {
 
     var radius = game.add.bitmapData(300, 300);
-    radius.circle(150, 150, 100, 'rgb(0,200,0)');
 
     baddy_radius = game.add.sprite(0, 0, radius);
-    game.physics.enable(baddy_radius, Phaser.Physics.ARCADE);
     baddy_radius.anchor.setTo(0.5, 0.5);
 
 
@@ -86,10 +86,9 @@ function add_enemies() {
     baddies_texture.ctx.fillStyle = '#dd0000';
     baddies_texture.ctx.fill();
 
-    baddy = game.add.sprite(50, 50, baddies_texture);
-    game.physics.enable(baddy, Phaser.Physics.ARCADE);
-    baddy.body.collideWorldBounds = true;
-    baddy.body.maxVelocity.y = 500;
+    baddy = game.add.sprite(150, 150, baddies_texture);
+    /*baddy.body.collideWorldBounds = true;
+    baddy.body.maxVelocity.y = 500;*/
 
     baddy_radius.x = baddy.x + (baddy.width / 2);
     baddy_radius.y = baddy.y + (baddy.height / 2);
