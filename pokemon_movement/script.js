6f07dff5196f619de2fbc16253ba4180bfeef012
ClementR <rondeau.clement@gmail.com>
2015-07-05 23:25:44 +0200

    Add animation stop when no input is given

--- pokemon_movement/script.js
+++ pokemon_movement/script.js
@@ -1,6 +1,6 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example',
                            {preload: preload, create: create, update: update});
-
+var prev_key = "down"
 function preload() {
     game.load.spritesheet('player', '../assets/images/pokemon_player.png', 32, 32, 12);
 }
@@ -20,11 +20,21 @@ function create() {
 function update() {
     if (cursors.down.isDown) {
         player.animations.play('walk_down', 5, true);
+        prev_key = "down";
     } else if (cursors.up.isDown) {
         player.animations.play('walk_up', 5, true);
+        prev_key = "up";
     } else if (cursors.left.isDown) {
         player.animations.play('walk_left', 5, true);
+        prev_key = "left";
     } else if (cursors.right.isDown) {
         player.animations.play('walk_right', 5, true);
+        prev_key = "right";
+    } else {
+        switch(prev_key) {
+            case "down":
+                player.animations.stop();
+            break;
+        }
     }
 }
