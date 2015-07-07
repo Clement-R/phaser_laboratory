9a44cc199a244a1a830146b3a3dc007c2c9370cf
Clement-R <rondeau.clement@gmail.com>
2015-07-07 16:40:09 +0200

    Add bullet in the game, gravity not good for the moment

--- friendly_fire/script.js
+++ friendly_fire/script.js
@@ -11,6 +11,7 @@ function preload() {
     game.load.image('body', '../assets/images/friendly_fire/body.png');
     game.load.image('arm', '../assets/images/friendly_fire/arm.png');
     game.load.image('gun', '../assets/images/friendly_fire/gun.png');
+    game.load.image('bullet', '../assets/images/friendly_fire/bullet.png');
 }
 
 function create() {
@@ -33,10 +34,57 @@ function create() {
                           player.y + (player.height / 3),
                           'arm');
     arm.anchor.setTo(0.5, 0);
+
+    create_controls();
+    create_bullets();
 }
 
 function update() {
     game.debug.spriteInfo(arm, 32, 32);
     arm.rotation = game.physics.arcade.angleToPointer(arm) - 1.5;
     gun.rotation = arm.rotation + 1.5;
+
+    if(spacebar.isDown && is_ready_to_fire) {
+        if(bullets.getFirstExists(false)) {
+            bullet = bullets.getFirstExists(false);
+            bullet.anchor.setTo(0.5, 0.5);
+            bullet.reset(gun.x + gun.width,
+                         gun.y);
+            bullet.body.gravity = 20;
+            bullet.body.velocity.x = 500;
+
+            is_ready_to_fire = false;
+            last_shot = game.time.now;
+        }
+    }
+    // Fire cooldown checker
+    if(game.time.now > last_shot + FIRERATE) {
+        is_ready_to_fire = true;
+    }
+}
+
+function create_bullets() {
+    FIRERATE = 200;
+    bullet_DAMAGE = 10;
+    is_ready_to_fire = true;
+    last_shot = 0;
+
+    bullets = game.add.group()
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(100, 'bullet');
+
+    bullets.forEach(function(bullet){
+        bullet.damage = bullet_DAMAGE;
+        bullet.checkWorldBounds = true;
+        bullet.outOfBoundsKill = true;
+    }, this);
+}
+
+function create_controls() {
+    up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
