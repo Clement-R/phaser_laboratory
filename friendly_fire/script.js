a6cef148a2b2ad2469182e9bc7ac2bd439ac9dc1
Clement-R <rondeau.clement@gmail.com>
2015-07-09 16:31:24 +0200

    Anchor point is strangely placed but position seems good

--- friendly_fire/script.js
+++ friendly_fire/script.js
@@ -26,110 +26,42 @@ function create() {
 
     g_player = game.add.group();
 
-    player = game.add.sprite(150, 383, 'body');
+    player = game.add.sprite(200, 150, 'body');
 
     arm = game.add.sprite(player.x + (player.width / 2),
                           player.y + (player.height / 3),
                           'arm');
-    arm.anchor.set(0.5, 0);
-
-    gun = game.add.sprite(arm.x,
-                          arm.y + arm.height,
-                          'gun');
-    gun.enableBody = true;
-    gun.anchor.set(0, 0.65);
-    // gun.visible = false;
-
+    arm.anchor.set(0.5, 0.1);
+    console.log(arm.anchor);
     g_player.add(player);
-    g_player.add(gun);
     g_player.add(arm);
 
     create_controls();
-    create_bullets();
-
-    arm.angle = -90;
-    gun.angle = arm.angle + 88.5;
-    var p = new Phaser.Point(arm.x, arm.y);
-    p.rotate(p.x, p.y, gun.rotation, false, 13);
-    gun.x = p.x;
-    gun.y = p.y;
 }
 
 function update() {
-    game.debug.spriteInfo(gun, 32, 32);
-    game.debug.pixel(gun.x, gun.y, 'green');
+    console.log(arm.x)
+    console.log(arm.y)
+    console.log(arm.anchor.x);
+    console.log(arm.anchor.y);
+    console.log(arm.width);
+    console.log(arm.height);
 
-    next_x = gun.x + 16 * Math.cos(gun.rotation);
-    next_y = gun.y + 10 * Math.sin(gun.rotation);
-    game.debug.pixel(next_x, next_y, 'pink');
-
-    // console.log(next_x - gun.x, next_y - gun.y);
+    console.log(arm.x + (arm.width * arm.anchor.x))
+    console.log(arm.y + (arm.height * arm.anchor.y))
+    game.debug.pixel(arm.x + (arm.width * arm.anchor.x),
+                     arm.y + (arm.height * arm.anchor.y),
+                     'yellow');
 
     // arm rotation
-    mouse_angle = (game.physics.arcade.angleToPointer(arm) * (180/PI)) - 86;
+    /*mouse_angle = (game.physics.arcade.angleToPointer(arm) * (180/PI)) - 86;
+    arm.angle = mouse_angle;*/
+    arm.angle += 2;
+    /*mouse_angle = (game.physics.arcade.angleToPointer(arm) * (180/PI)) - 86;
     if(mouse_angle < 10 && mouse_angle > -180) {
         arm.angle = mouse_angle;
-        var p = new Phaser.Point(arm.x, arm.y);
-        p.rotate(p.x, p.y, gun.rotation, false, 13);
-        gun.x = p.x;
-        gun.y = p.y;
-        gun.angle = arm.angle + 88.5;
     } else {
-        var p = new Phaser.Point(arm.x, arm.y);
-        p.rotate(p.x, p.y, gun.rotation, false, 13);
-        gun.x = p.x;
-        gun.y = p.y;
-        gun.angle = arm.angle + 88.5;
-    }
-
-    fire();
-}
-
-function fire() {
-    if(game.input.activePointer.isDown && is_ready_to_fire) {
-        if(bullets.getFirstExists(false)) {
-            bullet = bullets.getFirstExists(false);
-            // bullet.anchor.setTo(0.5, 0.5);
-
-            next_x = gun.x + 16 * Math.cos(gun.rotation);
-            next_y = gun.y + 10 * Math.sin(gun.rotation);
-
-            // Set the bullet position to the gun position
-            // bullet.reset(gun.x + 14, gun.y - 6);
-            bullet.reset(next_x, next_y);
-            bullet.rotation = gun.rotation;
-
-            // Shoot it in the right direction
-            bullet.body.velocity.x = Math.cos(bullet.rotation) * BULLET_SPEED;
-            bullet.body.velocity.y = Math.sin(bullet.rotation) * BULLET_SPEED;
-
-            is_ready_to_fire = false;
-            last_shot = game.time.now;
-        }
-    }
-    // Fire cooldown checker
-    if(game.time.now > last_shot + FIRERATE) {
-        is_ready_to_fire = true;
-    }
-}
-
-function create_bullets() {
-    FIRERATE = 400;
-    bullet_DAMAGE = 10;
-    is_ready_to_fire = true;
-    last_shot = 0;
-
-    bullets = game.add.group()
-    bullets.enableBody = true;
-    bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    bullets.createMultiple(100, 'bullet');
-
-    bullets.forEach(function(bullet){
-        bullet.damage = bullet_DAMAGE;
-        bullet.body.gravity.allowGravity = false;
-        bullet.checkWorldBounds = true;
-        bullet.outOfBoundsKill = true;
-    }, this);
+    }*/
 }
 
 function create_controls() {
