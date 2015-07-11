966dd2821547e9fa0f6b490ba53dc6281cc5b406
ClementR <rondeau.clement@gmail.com>
2015-07-11 18:31:33 +0200

    Bullet placement not working and bullet going wrong way

--- side_view_character/script.js
+++ side_view_character/script.js
@@ -8,6 +8,7 @@ http://stackoverflow.com/questions/2259476/rotating-a-point-about-another-point-
 */
 
 var PI = 3.14159265359;
+var BULLET_SPEED = 50;
 
 function preload() {
 }
@@ -36,23 +37,106 @@ function create() {
     arm.anchor.set(0.5, 0);
 
     arm.angle = -90;
+
+    var gun_texture = game.add.bitmapData(32, 96);
+    gun_texture.ctx.beginPath();
+    gun_texture.ctx.rect(0, 0, 32, 96);
+    gun_texture.ctx.fillStyle = "#f39c12";
+    gun_texture.ctx.fill();
+    var hand_anchor = get_hand_anchor();
+    gun = game.add.sprite(hand_anchor.x,
+                          hand_anchor.y,
+                          gun_texture);
+    gun.angle = arm.angle;
+    gun.anchor.set(0.5, 0);
+
+    create_bullets();
 }
 
 function update() {
-
     // arm rotation
     mouse_angle = (game.physics.arcade.angleToPointer(arm) * (180/PI)) - 86;
-    arm.angle = mouse_angle;
-    /*
     if(mouse_angle < 10 && mouse_angle > -180) {
         arm.angle = mouse_angle;
     }
-    */
 
+    // Place gun and make it rotate to follow the arm
+    var p = get_hand_anchor();
+    gun.angle = arm.angle;
+    gun.x = p.x;
+    gun.y = p.y;
+
+    fire();
+
+    /* ** DEBUG ** */
     // gun position and rotation
     game.debug.pixel(arm.x, arm.y, 'yellow');
-    // point to the hand
+    // placing gun in the hand
+    game.debug.pixel(p.x, p.y, 'red');
+    // Place bullet
+    // Place bullet
+    cannon_x = gun.x + gun.height * Math.cos(arm.rotation + 1.57);
+    cannon_y = gun.y + gun.height * Math.sin(arm.rotation + 1.57);
+    game.debug.pixel(cannon_x, cannon_y, 'white');
+    /* ** DEBUG ** */
+}
+
+function get_hand_anchor() {
     next_x = arm.x + arm.height * Math.cos(arm.rotation + 1.57);
     next_y = arm.y + arm.height * Math.sin(arm.rotation + 1.57);
-    game.debug.pixel(next_x, next_y, 'blue')
+    var point = new Phaser.Point(next_x, next_y);
+    return point;
+}
+
+function create_bullets() {
+    FIRERATE = 400;
+    bullet_DAMAGE = 10;
+    is_ready_to_fire = true;
+    last_shot = 0;
+
+    var bullet_texture = game.add.bitmapData(16, 32);
+    bullet_texture.ctx.beginPath();
+    bullet_texture.ctx.rect(0, 0, 16, 32);
+    bullet_texture.ctx.fillStyle = "#f1c40f";
+    bullet_texture.ctx.fill();
+
+    bullets = game.add.group()
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(100, bullet_texture);
+
+    bullets.forEach(function(bullet){
+        bullet.damage = bullet_DAMAGE;
+        bullet.body.gravity.allowGravity = false;
+        bullet.checkWorldBounds = true;
+        bullet.outOfBoundsKill = true;
+    }, this);
+}
+
+function fire() {
+    if(game.input.activePointer.isDown && is_ready_to_fire) {
+        if(bullets.getFirstExists(false)) {
+            bullet = bullets.getFirstExists(false);
+
+            // Place bullet
+            cannon_x = gun.x + gun.height * Math.cos(arm.rotation + 1.57);
+            cannon_y = gun.y + gun.height * Math.sin(arm.rotation + 1.57);
+
+            // Set the bullet position to the gun position
+            bullet.reset(cannon_x - bullet.height, cannon_y);
+            bullet.angle = gun.angle;
+
+            // Shoot it in the right direction
+            bullet.body.velocity.x = Math.cos(bullet.rotation) * BULLET_SPEED;
+            bullet.body.velocity.y = Math.sin(bullet.rotation) * BULLET_SPEED;
+
+            is_ready_to_fire = false;
+            last_shot = game.time.now;
+        }
+    }
+
+    // Fire cooldown checker
+    if(game.time.now > last_shot + FIRERATE) {
+        is_ready_to_fire = true;
+    }
 }
