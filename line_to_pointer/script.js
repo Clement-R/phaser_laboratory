12e10677192881f1ae1c3a6e615323f1528a73eb
ClementR <rondeau.clement@gmail.com>
2015-07-19 22:23:50 +0200

    Add rotation of sprite to follow drawed line

--- line_to_pointer/script.js
+++ line_to_pointer/script.js
@@ -8,14 +8,39 @@ function preload() {
 function create() {
     game.stage.backgroundColor = 0x2c3e50;
 
-    sprite = game.add.sprite(100, 100, 'star');
-    sprite.anchor.setTo(0.5, 0.5);
+    g = game.add.group();
+
+    sprite = game.add.sprite(350, 250, 'star');
+
+    arm_texture = game.add.bitmapData(32, 64);
+    arm_texture.ctx.beginPath();
+    arm_texture.ctx.rect(0, 0, 32, 64);
+    arm_texture.ctx.fillStyle = "#27ae60";
+    arm_texture.ctx.fill();
+    arm = game.add.sprite(sprite.x + sprite.width / 2,
+                          sprite.y + sprite.height / 2,
+                          arm_texture);
+    arm.anchor.set(0.5, 0);
+
+    // arm.angle = -90;
+
+    g.add(sprite);
+    g.add(arm);
 
     pointer = game.input.position;
-    line1 = new Phaser.Line(sprite.x, sprite.y, pointer.x, pointer.y);
+    line1 = new Phaser.Line(arm.x, arm.y, pointer.x, pointer.y);
 }
 
 function update() {
-    line1.setTo(sprite.x, sprite.y, pointer.x, pointer.y);
+    line1.setTo(arm.x, arm.y, pointer.x, pointer.y);
     game.debug.geom(line1);
+
+    x = arm.x - pointer.x;
+    y = arm.y - pointer.y;
+    arm.rotation = Math.atan2(y,x);
+    arm.angle += 90;
+
+    g.forEach(function(child){
+        // child.x += 0.5;
+    });
 }
