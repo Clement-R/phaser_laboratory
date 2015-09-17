7cdfb67670343984fe2e24b4bd17b9b68ea0e3c8
Clement-R <rondeau.clement@gmail.com>
2015-09-17 14:24:41 +0200

    Changed spawning algorithm

--- match3/script.js
+++ match3/script.js
@@ -33,9 +33,10 @@ function update() {
 function create_tiles() {
     create_tiles_textures();
 
-    for (var i = 0; i < ROW_COUNT; i++) {
+    // Random spawning algorithm, not good
+    for (var i = 0; i < COLUMN_COUNT; i++) {
         tiles[i] = [];
-        for (var j = 0; j < COLUMN_COUNT; j++) {
+        for (var j = 0; j < ROW_COUNT; j++) {
             var texture = game.rnd.pick(tiles_textures);
             console.log(texture);
             tiles[i][j] = game.add.sprite(i * CELL_SIZE_SPACED,
@@ -44,48 +45,63 @@ function create_tiles() {
         };
     };
 
-    game.input.onDown.add(pick_tile, this);
+    // Populate one cell at a time and check there is no match
+    for (var i = 0; i < COLUMN_COUNT; i++) {
+        tiles[i] = [];
+        for (var j = 0; j < ROW_COUNT; j++) {
+            /* TODO */
+
+            // First column of the row
+            if(i == 0) {
+                // First row on the board
+                if(j == 0) {
+                    // First row of the board
+                    var texture = game.rnd.pick(tiles_textures);
+                } else if (j == (ROW_COUNT - 1)) {
+                    // Last row of the board
+
+                    /* Check neighbors cells on top and right */
+                } else {
+                    // Other rows
+
+                    /* Check neighbors cells on top, bottom and right */
+                }
+            // Last colum of the row
+            } else if(i == (COLUMN_COUNT - 1)) {
+
+            } else {
+                //
+            }
+
+            /* /TODO */
+            tiles[i][j] = game.add.sprite(i * CELL_SIZE_SPACED,
+                                          j * CELL_SIZE_SPACED,
+                                          texture);
+        };
+    };
+
+    // game.input.onDown.add(pick_tile, this);
 }
 
 function create_tiles_textures() {
     red_tile = game.add.bitmapData(64, 64);
     red_tile.ctx.beginPath();
     red_tile.ctx.rect(0,0,64,64);
-    red_tile.ctx.fillStyle = '#ff0000';
+    red_tile.ctx.fillStyle = '#1abc9c';
     red_tile.ctx.fill();
     tiles_textures.push(red_tile);
 
     green_tile = game.add.bitmapData(64, 64);
     green_tile.ctx.beginPath();
     green_tile.ctx.rect(0,0,64,64);
-    green_tile.ctx.fillStyle = '#00ff00';
+    green_tile.ctx.fillStyle = '#3498db';
     green_tile.ctx.fill();
     tiles_textures.push(green_tile);
 
     blue_tile = game.add.bitmapData(64, 64);
     blue_tile.ctx.beginPath();
     blue_tile.ctx.rect(0,0,64,64);
-    blue_tile.ctx.fillStyle = '#0000ff';
+    blue_tile.ctx.fillStyle = '#e74c3c';
     blue_tile.ctx.fill();
     tiles_textures.push(blue_tile);
-}
-
-function pick_tile(){
-    // Saving input coordinates
-    start_x = game.input.worldX;
-    start_y = game.input.worldY;
-    // Saving row and column numbers
-    moving_row = Math.floor(start_y / CELL_SIZE);
-    moving_col = Math.floor(start_x / CELL_SIZE);
-
-    // now dragging is allowed
-    DRAGGING = true;
-
-    // updating listeners
-    game.input.onDown.remove(pick_tile, this);
-    game.input.onUp.add(release_tile, this);
-}
-
-function release_tile() {
-
 }
