7fce3cc7209dafe969ed5e3483aaffc7ddcc12dd
Clement-R <rondeau.clement@gmail.com>
2015-09-22 14:02:54 +0200

    Add comments on how to check neighbors for each case

--- match3/script.js
+++ match3/script.js
@@ -34,49 +34,71 @@ function create_tiles() {
     create_tiles_textures();
 
     // Random spawning algorithm, not good
-    for (var i = 0; i < COLUMN_COUNT; i++) {
-        tiles[i] = [];
-        for (var j = 0; j < ROW_COUNT; j++) {
-            var texture = game.rnd.pick(tiles_textures);
-            console.log(texture);
-            tiles[i][j] = game.add.sprite(i * CELL_SIZE_SPACED,
-                                          j * CELL_SIZE_SPACED,
-                                          texture);
-        };
-    };
+    // for (var i = 0; i < COLUMN_COUNT; i++) {
+    //     tiles[i] = [];
+    //     for (var j = 0; j < ROW_COUNT; j++) {
+    //         var texture = game.rnd.pick(tiles_textures);
+    //         console.log(texture);
+    //         tiles[i][j] = game.add.sprite(i * CELL_SIZE_SPACED,
+    //                                       j * CELL_SIZE_SPACED,
+    //                                       texture);
+    //     };
+    // };
 
     // Populate one cell at a time and check there is no match
     for (var i = 0; i < COLUMN_COUNT; i++) {
         tiles[i] = [];
         for (var j = 0; j < ROW_COUNT; j++) {
-            /* TODO */
 
-            // First column of the row
+            var texture = "";
+
+            // First column of the board
             if(i == 0) {
                 // First row on the board
                 if(j == 0) {
-                    // First row of the board
+                    // First cell of the board
                     var texture = game.rnd.pick(tiles_textures);
                 } else if (j == (ROW_COUNT - 1)) {
                     // Last row of the board
-
                     /* Check neighbors cells on top and right */
+                    var texture = game.rnd.pick(tiles_textures);
                 } else {
                     // Other rows
-
                     /* Check neighbors cells on top, bottom and right */
+                    var texture = game.rnd.pick(tiles_textures);
                 }
-            // Last colum of the row
+            // Last colum of the board
             } else if(i == (COLUMN_COUNT - 1)) {
-
+                // First row on the board
+                if(j == 0) {
+                    /* Check neighbors cells on left and bottom */
+                    var texture = game.rnd.pick(tiles_textures);
+                } else if (j == (ROW_COUNT -1)) {
+                    /* Check neighbors cells on top and left */
+                    var texture = game.rnd.pick(tiles_textures);
+                } else {
+                    /* Check neighbors cells on top, left and bottom */
+                    var texture = game.rnd.pick(tiles_textures);
+                }
             } else {
-                //
+                // First row on the board
+                if(j == 0) {
+                    /* Check neighbors on left, right and bottom */
+                    var texture = game.rnd.pick(tiles_textures);
+                } else if (j == (ROW_COUNT -1)) {
+                    /* Check neighbors on top, left and right */
+                    var texture = game.rnd.pick(tiles_textures);
+                } else {
+                    /* Check neighbors on all sides */
+                }
             }
 
-            /* /TODO */
-            tiles[i][j] = game.add.sprite(i * CELL_SIZE_SPACED,
-                                          j * CELL_SIZE_SPACED,
-                                          texture);
+            if(texture != "") {
+                // var texture = game.rnd.pick(tiles_textures);
+                tiles[i][j] = game.add.sprite(i * CELL_SIZE_SPACED,
+                                              j * CELL_SIZE_SPACED,
+                                              texture);
+            }
         };
     };
 
@@ -89,6 +111,7 @@ function create_tiles_textures() {
     red_tile.ctx.rect(0,0,64,64);
     red_tile.ctx.fillStyle = '#1abc9c';
     red_tile.ctx.fill();
+    red_tile.tile_id = "0";
     tiles_textures.push(red_tile);
 
     green_tile = game.add.bitmapData(64, 64);
@@ -96,6 +119,7 @@ function create_tiles_textures() {
     green_tile.ctx.rect(0,0,64,64);
     green_tile.ctx.fillStyle = '#3498db';
     green_tile.ctx.fill();
+    green_tile.tile_id = "1";
     tiles_textures.push(green_tile);
 
     blue_tile = game.add.bitmapData(64, 64);
@@ -103,5 +127,6 @@ function create_tiles_textures() {
     blue_tile.ctx.rect(0,0,64,64);
     blue_tile.ctx.fillStyle = '#e74c3c';
     blue_tile.ctx.fill();
+    blue_tile.tile_id = "2";
     tiles_textures.push(blue_tile);
 }
