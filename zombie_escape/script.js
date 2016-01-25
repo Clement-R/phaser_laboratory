c4facd607618aab90c13bb3409f8ee4f2b04b96d
Clement-R <rondeau.clement@gmail.com>
2016-01-25 22:16:05 +0100

    Rework project to remove P2, add aggro detection and zombie moving to player when detected (arcade physic)

--- zombie_escape/script.js
+++ zombie_escape/script.js
@@ -1,11 +1,8 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example',
                            {preload: preload, create: create, update: update});
-
-var collisionCircle;
-
 /*
     TO DO :
-    Move to player whean player enter in aggro
+    Move to player when player enter in aggro
     http://www.html5gamedevs.com/topic/4826-help-arcade-physics-with-p2-objects/?p=29613
 
     Move radius with baddy
@@ -22,88 +19,30 @@ function preload() {
 }
 
 function create() {
-    game.physics.startSystem(Phaser.Physics.P2JS);
-
-    game.physics.p2.gravity.y = 0;
-
     game.stage.backgroundColor = 0x2c3e50;
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
     add_safe_zone();
     add_enemies();
     add_player();
-
-    // game.physics.p2.enable(player, true);
-    game.physics.p2.enable(player);
-    player.body.fixedRotation = true;
-    player.name = "player";
-    player.body.collideWorldBounds = true;
-
-    // game.physics.p2.enable(baddy, true);
-    game.physics.p2.enable(baddy);
-    //baddy.body.fixedRotation = true;
-    baddy.name = "baddy";
-    baddy.body.collideWorldBounds = true;
-
-    // game.physics.p2.enable(baddy_radius, true);
-    game.physics.p2.enable(baddy_radius, true);
-    baddy_radius.name = "baddy_radius";
-    baddy_radius.body.setCircle(200);
-    baddy_radius.body.fixedRotation = true;
-
-    game.physics.p2.setPostBroadphaseCallback(checkOverlap, this);
-}
-
-function checkOverlap(body1, body2) {
-    if (body1.sprite.name === 'player' && body2.sprite.name === 'baddy_radius') {
-        moveBaddy(baddy);
-        return false;
-    }
-    if (body1.sprite.name === 'baddy_radius' && body2.sprite.name === 'player') {
-        moveBaddy(baddy);
-        return false;
-    }
-    if ((body1.sprite.name === 'baddy' && body2.sprite.name === 'baddy_radius') ||
-        (body1.sprite.name === 'baddy_radius' && body2.sprite.name === 'baddy')){
-        return false;
-    }
-    if ((body1.sprite.name === 'baddy' && body2.sprite.name === 'player') ||
-        (body1.sprite.name === 'player' && body2.sprite.name === 'baddy')){
-        return true;
-    }
-    return true;
 }
 
 function update() {
-    player.body.setZeroVelocity();
-
-    baddy_radius.body.x = baddy.body.x;
-    baddy_radius.body.y = baddy.body.y;
-
     if (cursors.left.isDown) {
-        player.body.moveLeft(300);
+        player.x -= 2;
     }
     if (cursors.right.isDown) {
-        player.body.moveRight(300);
+        player.x += 2;
     }
     if (cursors.up.isDown) {
-        player.body.moveUp(300);
+        player.y -= 2;
     }
     if (cursors.down.isDown) {
-        player.body.moveDown(300);
+        player.y += 2;
     }
-}
 
-function moveBaddy (baddy) {
-     accelerateToObject(baddy, player, 100);
-}
-
-function accelerateToObject(obj1, obj2, speed) {
-    console.log(obj1);
-    if (typeof speed === 'undefined') { speed = 60; }
-    var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
-    obj1.body.rotation = angle + game.math.degToRad(90);
-    obj1.body.force.x = Math.cos(angle) * speed;
-    obj1.body.force.y = Math.sin(angle) * speed;
+    baddy.aggro();
 }
 
 function add_safe_zone() {
@@ -123,18 +62,11 @@ function add_player() {
     player_texture.ctx.fill();
 
     player = game.add.sprite(520, 340, player_texture);
-    /*player.body.collideWorldBounds = true;
-    player.body.maxVelocity.y = 500;*/
+    player.anchor.setTo(0.5, 0.5);
     cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    sprintButton = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
 }
 
 function add_enemies() {
-    var radius = game.add.bitmapData(300, 300);
-    baddy_radius = game.add.sprite(0, 0, radius);
-    baddy_radius.anchor.setTo(0.5, 0.5);
-
     var baddies_texture = game.add.bitmapData(32, 32);
     baddies_texture.ctx.beginPath();
     baddies_texture.ctx.rect(0,0,32,32);
@@ -142,7 +74,18 @@ function add_enemies() {
     baddies_texture.ctx.fill();
 
     baddy = game.add.sprite(150, 150, baddies_texture);
+    baddy.anchor.setTo(0.5, 0.5);
 
-    baddy_radius.x = baddy.x;
-    baddy_radius.y = baddy.y;
+    game.physics.arcade.enable(baddy, Phaser.Physics.ARCADE);
+
+    baddy.aggro = function() {
+        var distance = Math.sqrt(Math.pow((player.x - baddy.x), 2) + Math.pow((player.y - baddy.y), 2));
+        if(distance <= 150) {
+            console.log("BRAIN !");
+            game.physics.arcade.moveToObject(baddy,
+                                             player,
+                                             5,
+                                             1000);
+        }
+    };
 }
