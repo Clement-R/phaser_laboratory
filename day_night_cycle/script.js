e35ff1b6584b8b993815a70903d2b15fbf7f698a
Clement-R <rondeau.clement@gmail.com>
2016-01-26 14:07:10 +0100

    day_night_cycle project is now working

--- day_night_cycle/script.js
+++ day_night_cycle/script.js
@@ -1,13 +1,25 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example',
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "phaser-example",
                            {preload: preload, create: create, update: update});
 
 function preload() {
-    game.load.image('tree', '../assets/images/trees_1.png');
+    game.load.image("background", "../assets/images/colored_forest_800_600.png");
+    game.load.image("nightFilter", "../assets/images/dark_blue.png");
 }
 
 function create() {
     game.stage.backgroundColor = 0x2c3e50;
-    game.add.sprite(0, 0, 'tree');
+    var background = game.add.sprite(0, 0, "background");
+    var nightFilter = game.add.sprite(0, 0, "nightFilter")
+
+    console.log(nightFilter);
+
+    var tweenFall = game.add.tween(nightFilter).to({alpha: 0},
+                                                   5000,
+                                                   Phaser.Easing.Linear.In,
+                                                   true,
+                                                   0,
+                                                   -1,
+                                                   true);
 }
 
 function update() {
