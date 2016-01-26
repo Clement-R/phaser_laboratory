18ef3fbcbc3e9121da52a893a267cad228477722
Clement-R <rondeau.clement@gmail.com>
2016-01-26 19:21:49 +0100

    Now switch between day and night with a timer

--- day_night_cycle/script.js
+++ day_night_cycle/script.js
@@ -9,18 +9,32 @@ function preload() {
 function create() {
     game.stage.backgroundColor = 0x2c3e50;
     var background = game.add.sprite(0, 0, "background");
-    var nightFilter = game.add.sprite(0, 0, "nightFilter")
+    nightFilter = game.add.sprite(0, 0, "nightFilter");
 
-    console.log(nightFilter);
+    isNight = true;
 
-    var tweenFall = game.add.tween(nightFilter).to({alpha: 0},
-                                                   5000,
-                                                   Phaser.Easing.Linear.In,
-                                                   true,
-                                                   0,
-                                                   -1,
-                                                   true);
+    tweenRise = game.add.tween(nightFilter).to({alpha: 0},
+                                                   2500,
+                                                   Phaser.Easing.Linear.In);
+    tweenFall = game.add.tween(nightFilter).to({alpha: 1},
+                                                   2500,
+                                                   Phaser.Easing.Linear.In);
+    timer = game.time.create(false);
+    timer.loop(5000, toggleDay, this);
+    timer.start();
 }
 
 function update() {
+    console.log(nightFilter.alpha);
+}
+
+function toggleDay() {
+    console.log("Toggle");
+    if(isNight) {
+        tweenRise.start();
+        isNight = false;
+    } else {
+        tweenFall.start();
+        isNight = true;
+    }
 }
