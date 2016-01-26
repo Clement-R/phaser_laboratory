a1bab3a067296750664e86d4f667125f6a62dff1
Clement-R <rondeau.clement@gmail.com>
2016-01-26 19:45:16 +0100

    Rework a comment

--- basic_particles/script.js
+++ basic_particles/script.js
@@ -33,9 +33,11 @@ function particleBurst(pointer) {
     emitter.x = pointer.x;
     emitter.y = pointer.y;
 
-    //  The first parameter sets the effect to "explode" which means all particles are emitted at once
-    //  The second gives each particle a 2000ms lifespan
-    //  The third is ignored when using burst/explode mode
-    //  The final parameter (10) is how many particles will be emitted in this single burst
+    //  The first parameter sets the effect to "explode"
+    //  which means all particles are emitted at once.
+    //  The second gives each particle a 2000ms lifespan.
+    //  The third is ignored when using burst/explode mode.
+    //  The final parameter (10) is how many particles will
+    //  be emitted in this single burst.
     emitter.start(true, 2000, null, 50);
 }
